bc2b2f60b0a50abe9d0d9c45d8dc36f55b46d5b7
“lavaughncampbell” <lavaughnccampbell@gmail.com>
2020-05-02 23:10:18 -0500

    it renders a post

---
+++ react-dev-pool/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import PostContainer from './PostContainer'
+
+function App() {
+  return (
+    <div className="App">
+      <PostContainer></PostContainer>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-dev-pool/src/PostContainer/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+// you will see (Default) export written here often instead of at the bottom 
+export default class PostContainer extends Component {
+
+	render() {
+		return(
+			<h2>Post Container</h2>
+		)
+	}
+}
