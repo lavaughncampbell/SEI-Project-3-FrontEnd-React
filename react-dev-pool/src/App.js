96fef5b4e9773b281e200049528345851ef7388e
“lavaughncampbell” <lavaughnccampbell@gmail.com>
2020-05-03 10:23:41 -0500

    register route using API

--- react-dev-pool/src/App.js
+++ react-dev-pool/src/App.js
@@ -1,8 +1,49 @@
 import React from 'react';
 import './App.css';
 import PostContainer from './PostContainer'
+import LoginRegisterForm from './LoginRegisterForm'
+import Header from './Header'
+
+
+
+export default class App extends Component {
+
+	constructor() {
+		super()
+
+		this.state = {
+			loggedIn: false, 
+			loggedInUserEmail: 
+		}
+	}
+
+	register = async (registerInfo) => {
+		console.log("register() in App.js called with the following info", registerInfo); 
+		const url = process.env.REACT_APP_API_URL + "/api/v1/users/register"
+
+		try {
+			const registerResponse = await fetch(url, {
+				// now that our back end has sessions and is expecting cookies 
+				// INCLUDE THIS crendentials: 'include' in every fetch call
+				// it will send your cookie
+				// in unit 2 this was being done automatically for you by the browser
+				// IF YOU LEAVE IT OUT, YOU WILL NOT BE AUTHENTICATED 
+				crendentials: 'include', 
+				method: 'POST', 
+				body: JSON.stringify(registerInfo), 
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			})
+			console.log("registerResponse", registerResponse); 
+			const registerJson = await registerResponse.json()
+			console.log("registerJson", registerJson); 
+		}
+	}
+}
 
 function App() {
+	console.log(process.env)
   return (
     <div className="App">
       <PostContainer></PostContainer>
--- react-dev-pool/src/PostContainer/index.js
+++ react-dev-pool/src/PostContainer/index.js
@@ -3,6 +3,38 @@ import React, { Component } from 'react'
 // you will see (Default) export written here often instead of at the bottom 
 export default class PostContainer extends Component {
 
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			dogs: []
+		}
+	}
+
+	componentDidMount() {
+		// get the posts when this component is first rendered
+		this.getPosts()
+	}
+
+	getPosts = async () => {
+		try {
+			// load posts from the post index route in our API
+			// be sure to add a / to the of this url 
+			// Flask/Flask - Cors expects this!!!
+			const url = process.env.REACT_APP_API_URL + " /api/v1/posts/"
+			console.log("about to fetch data from:");
+			console.log(url);
+			const postResponse = await fetch(url)
+			console.log("here is the Response from the fetch call:");
+			console.log(postResponse);
+			const postsJson = await postResponse.json()
+			console.log("here is the data we got in getPosts in PostContainer")
+			console.log(postsJson);
+		} catch(err) {
+			console.error("Error getting dog data", err)
+		}
+	}
+
 	render() {
 		return(
 			<h2>Post Container</h2>
