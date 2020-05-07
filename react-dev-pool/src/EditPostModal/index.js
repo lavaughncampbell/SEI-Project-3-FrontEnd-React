d9063c1390caeb2a70effed4573225443984945f
“lavaughncampbell” <lavaughnccampbell@gmail.com>
2020-05-06 23:27:31 -0500

    edit conditionally renders properly

--- react-dev-pool/src/EditPostModal/index.js
+++ react-dev-pool/src/EditPostModal/index.js
@@ -1,7 +1,37 @@
 import React, { Component } from 'react'
+import { Form, Button, Label, Segment } from 'semantic-ui-react'
 
-export default class EditDogModal extends Component {
+export default class EditPostModal extends Component {
+	constructor(props) {
+    super(props)
+
+    this.state = {
+      description: '',
+      comment: ''
+    }
+  }
+
   render() {
-    return <h2>Edit Dog Modal</h2>
+    return(
+      <Segment> 
+        <h3>Enter new info</h3>
+        <Form>
+          <Form.Input 
+            type="text"
+            name="description"
+            value={this.state.description}
+            placeholder="What's on your mind?"
+          />
+          <Label>Breed:</Label>
+          <Form.Input 
+            type="text"
+            name="comment"
+            value={this.state.comment}  
+            placeholder="Comment Here"
+          />
+          <Button type="Submit">Update Post</Button>
+        </Form>
+      </Segment>
+    )    
   }
 } 
